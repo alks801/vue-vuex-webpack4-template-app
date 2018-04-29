24b075f893bd79d646e910afb5fcf8e3f4e73e6e
sn0wil <joukeery@gmail.com>
2018-04-29 17:58:35 +0300

    step 2. Webpack-dev-server with js inject and hot reload

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -15,7 +17,17 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    hot: true
+  },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true,
+      hash: true
+    }),
   ]
 }
